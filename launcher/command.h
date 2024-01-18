6b59daee84b0f393150afdd2e6a80645183f0c3c
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-01-18 23:13:50 +0900

    launcherの画面改善

--- launcher/command.h
+++ launcher/command.h
@@ -9,16 +9,20 @@ struct Command {
     Command(WebCFace::Client &wcli, const std::string &name,
             const std::string &exec, const std::string &workdir)
         : name(name), exec(exec), workdir(workdir) {
+        auto read_log = [this](const char *bytes, std::size_t n) {
+            this->logs.append(bytes, n);
+        };
         start = wcli.func(name + "_start")
-                    .set([this] {
+                    .set([this, read_log] {
                         if (is_running()) {
                             spdlog::warn("Command '{}' is already started.",
                                          this->name);
                             throw std::runtime_error("already started");
                         } else {
                             spdlog::info("Starting command '{}'.", this->name);
+                            this->logs.clear();
                             p = std::make_shared<TinyProcessLib::Process>(
-                                this->exec, this->workdir);
+                                this->exec, this->workdir, read_log, read_log);
                         }
                     })
                     .hidden(true);
@@ -43,5 +47,6 @@ struct Command {
     int exit_status = 0;
     std::shared_ptr<TinyProcessLib::Process> p;
     WebCFace::Func start, terminate;
+    std::string logs;
     bool is_running() { return p && !p->try_get_exit_status(exit_status); }
 };
