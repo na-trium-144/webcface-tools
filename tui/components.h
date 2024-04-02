9209951b4b72b34a863f7a20c0125730946a4a45
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-04-03 04:05:38 +0900

    viewContainer

--- tui/components.h
+++ tui/components.h
@@ -53,3 +53,100 @@ inline void addTextComponent(ftxui::ScreenInteractive &screen,
         });
     }
 }
+
+class ViewUIContainer : public ftxui::ComponentBase {
+    webcface::View view;
+    std::unordered_map<webcface::ViewComponent::Id, ftxui::Component>
+        ui_components;
+
+    // childrenにレイアウトを反映 (renderはしない)
+    // childrenは2次元vectorのような感じになる
+    // 要素が足りなければAddし、多ければDetachする
+    void updateLayout() {
+        auto &root = this->ChildAt(0);
+        // childrenを空にする (行は削除しない)
+        for (std::size_t row = 0; row < root->ChildCount(); row++) {
+            root->ChildAt(row)->DetachAllChildren();
+        }
+        if (root->ChildCount() == 0) {
+            root->Add(ftxui::Container::Horizontal({}));
+        }
+        std::size_t row = 0;
+        for (const auto &cp : this->view.get()) {
+            switch (cp.type()) {
+            case webcface::ViewComponentType::text:
+                break;
+            case webcface::ViewComponentType::new_line:
+                if (root->ChildAt(row)->ChildCount() > 0) {
+                    row++;
+                }
+                if (root->ChildCount() <= row) {
+                    root->Add(ftxui::Container::Horizontal({}));
+                }
+                break;
+            case webcface::ViewComponentType::button:
+                if (!this->ui_components.count(
+                        cp.id()) /* && cp has changed */) {
+                    this->ui_components[cp.id()] = ftxui::Button({
+                        .text = cp.text(),
+                        // todo: color
+                    });
+                }
+                ftxui::Component ui_cp = this->ui_components[cp.id()];
+                root->ChildAt(row)->Add(ui_cp);
+            }
+        }
+    }
+
+  public:
+    explicit ViewUIContainer(const webcface::View &view)
+        : ftxui::ComponentBase(), view(view), row_selector(0), col_selectors() {
+        this->Add(ftxui::Container::Vertical({}));
+        view.prependListener([this] { this->updateLayout(); });
+    }
+    ~ViewUIContainer() override {}
+    ViewUIContainer(const ViewUIContainer &) = delete;
+    ViewUIContainer(ViewUIContainer &&) = delete;
+    ViewUIContainer &operator=(const ViewUIContainer &) = delete;
+    ViewUIContainer &operator=(ViewUIContainer &&) = delete;
+
+    // renderする (childrenのレイアウトは無視)
+    ftxui::Element Render() override {
+        std::vector<ftxui::Element> elements, elements_col;
+        for (const auto &cp : this->view.get()) {
+            switch (cp.type()) {
+            case webcface::ViewComponentType::text:
+                elements_col.push_back(ftxui::text(cp.text()));
+                break;
+            case webcface::ViewComponentType::new_line:
+                elements.push_back(ftxui::hbox(elements_col));
+                elements_col.clear();
+                break;
+            case webcface::ViewComponentType::button:
+                ftxui::Component ui_cp = this->ui_components[cp.id()];
+                if (ui_cp) {
+                    elements.push_back(ui_cp->Render());
+                }
+                break;
+            }
+        }
+        return ftxui::vbox(elements);
+    }
+};
+inline ftxui::Component viewComponent(const webcface::View &view) {
+    return std::make_shared<ViewUIContainer>(view);
+}
+inline void addViewComponent(ftxui::ScreenInteractive &screen,
+                             ftxui::Component &container,
+                             const webcface::View &view) {
+    if (view.tryGet()) {
+        container->Add(viewComponent(view));
+    } else {
+        auto handle = std::make_shared<webcface::View::EventHandle>();
+        *handle = view.prependListener([&screen, &container, view, handle] {
+            screen.Post(
+                [&container, view] { container->Add(viewComponent(view)); });
+            view.removeListener(*handle);
+        });
+    }
+}
