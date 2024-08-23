f94bc92269215d69eb4922750fb43c41b352e900
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-08-24 00:34:25 +0900

    lsもコンパイルは通した

--- ls/main.cc
+++ ls/main.cc
@@ -3,7 +3,6 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <spdlog/spdlog.h>
-#include <eventpp/utilities/counterremover.h>
 #include <string>
 #include <vector>
 #include <thread>
@@ -20,72 +19,19 @@ int main(int argc, char **argv) {
                    "Server address (default: 127.0.0.1)");
     app.add_option("-p,--port", wcli_port,
                    "Server port (default: " WEBCFACE_DEFAULT_PORT_S ")");
-    app.add_option("-t,--timeout", timeout,
-                   "Timeout for receiving entry information (default: 10)");
+    // app.add_option("-t,--timeout", timeout,
+    //                "Timeout for receiving entry information (default: 10)");
 
     CLI11_PARSE(app, argc, argv);
 
     webcface::Client wcli("", wcli_host, wcli_port);
     wcli.autoReconnect(false);
-
-    static std::mutex timeout_m;
-    static auto timeout_begin = std::chrono::steady_clock::now();
-    wcli.onMemberEntry().appendListener([](const webcface::Member &m) {
-        eventpp::counterRemover(m.onPing().callbackList())
-            .append([](const auto &) {
-                std::lock_guard lock(timeout_m);
-                timeout_begin = std::chrono::steady_clock::now();
-            });
-        m.onValueEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onTextEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onFuncEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onViewEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onImageEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onCanvas2DEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onCanvas3DEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-        m.onRobotModelEntry().callbackList().append([](const auto &) {
-            std::lock_guard lock(timeout_m);
-            timeout_begin = std::chrono::steady_clock::now();
-        });
-    });
     wcli.waitConnection();
 
     if (!wcli.connected()) {
         spdlog::error("could not connect to webcface server");
         return 1;
     }
-
-    std::chrono::milliseconds wait_dur;
-    do {
-        {
-            std::lock_guard lock(timeout_m);
-            wait_dur = std::chrono::milliseconds(timeout) -
-                       std::chrono::duration_cast<std::chrono::milliseconds>(
-                           std::chrono::steady_clock::now() - timeout_begin);
-        }
-        std::this_thread::sleep_for(wait_dur);
-    } while (wait_dur.count() > 0);
 
     std::vector<ftxui::Element> elem_members;
     for (const auto &m : wcli.members()) {
