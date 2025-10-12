8e6ab5afa6199f95d68bd5162085db2efbe89295
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-10-13 02:54:33 +0900

    add tiny-process-library to default.nix

--- default.nix
+++ default.nix
@@ -9,6 +9,18 @@ let
     sha256 = "sha256-jZ/w1Qn3kU0+gHBoppXeI0irfGaFNmjXp3tr7zgJYKE=";
   };
   webcface = pkgs.callPackage (webcface-src + "/default.nix") { inherit pkgs; };
+
+  tiny-process-library = pkgs.stdenv.mkDerivation {
+    pname = "tiny-process-library";
+    version = "v2.0.4-29";
+    src = pkgs.fetchFromGitLab {
+      owner = "eidheim";
+      repo = "tiny-process-library";
+      rev = "8bbb5a211c5c9df8ee69301da9d22fb977b27dc1";
+      sha256 = "sha256-EdaPXKHbAMR2M2FwPnDP+KeuYbGfGE2j5QXB+CyyjnM=";
+    };
+    nativeBuildInputs = [ pkgs.cmake ];
+  };
 in
 pkgs.stdenv.mkDerivation rec {
   pname = "webcface-tools";
@@ -29,8 +41,10 @@ pkgs.stdenv.mkDerivation rec {
     pkgs.SDL2
     pkgs.tomlplusplus
     pkgs.cli11
-    # pkgs.fmt_11
-    # spdlog
+    tiny-process-library
+    # from webcface:
+      # pkgs.fmt_11
+      # spdlog
   ];
 
   mesonFlags = [];
